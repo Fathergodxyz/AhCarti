2955055c422e5393b881cccacf895d3ebc035412
Fathergodxyz <fathergodxyz@proton.me>
2024-11-19 07:03:05 -0500

    Update VALUES.ts

--- src/constants/VALUES.ts
+++ src/constants/VALUES.ts
@@ -1,6 +1,6 @@
 export const GOAL_MARKET_CAP = 1000000;
 export const BLESSINGS = 2.5;
 export const GRAPH_URL =
-  "https://dexscreener.com/solana/txg2f4ihGQeyX4iPY5thkSUzQUmZYwYWY6T1SiUFsnz?embed=1&loadChartSettings=0&trades=0&info=0&chartLeftToolbar=0&chartTheme=dark&theme=dark&chartStyle=0&chartType=usd&interval=15";
+  "<style>#dexscreener-embed{position:relative;width:100%;padding-bottom:125%;}@media(min-width:1400px){#dexscreener-embed{padding-bottom:65%;}}#dexscreener-embed iframe{position:absolute;width:100%;height:100%;top:0;left:0;border:0;}</style><div id="dexscreener-embed"><iframe src="https://dexscreener.com/solana/txg2f4ihGQeyX4iPY5thkSUzQUmZYwYWY6T1SiUFsnz?embed=1&loadChartSettings=0&trades=0&tabs=0&info=0&chartLeftToolbar=0&chartTimeframesToolbar=0&loadChartSettings=0&chartDefaultOnMobile=1&chartTheme=dark&theme=dark&chartStyle=13&chartType=marketCap&interval=240"></iframe></div>";
 export const DUE_DATE = "2024-12-19T06:47:59Z"; // ISO 8601 format with UTC timezone
 export const TIME_OVER_MESSAGE = "Time is over!";
