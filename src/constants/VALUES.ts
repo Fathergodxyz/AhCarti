cde170386134f74658df6efe7c3d5d050bb1e371
fathergodxyz <fathergodxyz@proton.me>
2024-11-22 03:04:02 -0500

    hanging time value

--- src/constants/VALUES.ts
+++ src/constants/VALUES.ts
@@ -1,5 +1,5 @@
 export const GOAL_MARKET_CAP = 100000;
 export const BLESSINGS = 500;
 export const GRAPH_URL ="https://dexscreener.com/solana/txg2f4ihGQeyX4iPY5thkSUzQUmZYwYWY6T1SiUFsnz?embed=1&loadChartSettings=0&trades=0&tabs=0&info=0&chartLeftToolbar=0&chartTimeframesToolbar=0&loadChartSettings=0&chartTheme=dark&theme=dark&chartStyle=2&chartType=marketCap&interval=15"
-export const DUE_DATE = "2024-11-28T06:47:59Z"; // ISO 8601 format with UTC timezone
+export const DUE_DATE = "2024-11-29T03:30:00Z"; // ISO 8601 format with UTC timezone
 export const TIME_OVER_MESSAGE = "Time is over!";
